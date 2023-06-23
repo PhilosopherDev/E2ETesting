11cba7c4f31b1b51ebf7c2ecd1d8c93d9fac1e70
BYUNGMIN CHOI <nekarakube@gmail.com>
2023-06-24 08:08:13 +0900

    should 사용법, implicit, explicit

--- code/02 Basics/01 Starting Project/cypress/e2e/basics.cy.js
+++ code/02 Basics/01 Starting Project/cypress/e2e/basics.cy.js
@@ -8,6 +8,8 @@ describe('tasks page', () => {
 
   it('should display the page title', () => {
     cy.visit('http://localhost:5173/');
+    cy.get('h1').should('exist');
+    cy.get('h1').should('have.length', 1);
     cy.get('h1').contains('My Cypress Course Tasks');
   });
 })
