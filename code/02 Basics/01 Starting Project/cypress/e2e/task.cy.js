57d73b06cac0e46d5c66776f03399b8a13f52cb4
BYUNGMIN CHOI <nekarakube@gmail.com>
2023-06-26 07:04:54 +0900

    Cancel 눌러서 모달 닫기 확인 추가, should('not.exist') 를 통해 정확한 확인

--- code/02 Basics/01 Starting Project/cypress/e2e/task.cy.js
+++ code/02 Basics/01 Starting Project/cypress/e2e/task.cy.js
@@ -3,10 +3,17 @@
 describe('tasks management, user interaction', () => {
     it('should open and close the new task modal', () => {
         cy.visit('http://localhost:5173/');
+
+        // Backdrop 눌러서 Modal 닫기 처리 후 확인
         cy.contains('Add Task').click();
-        // cy.contains('Cancel').click();
         // cy.get('.backdrop').click(); // 이건 실패한다. 무조건 중앙을 클릭하는데 backdrop이 아니라 다른 element를 클릭해서 그렇다.
         cy.get('.backdrop').click({ force: true });
+        cy.get('.backdrop').should('not.exist');
+        cy.get('dialog.modal').should('not.exist');
+
+        // Cancel 눌러서 Modal 닫기 처리 후 확인
+        cy.contains('Add Task').click();
+        cy.contains('Cancel').click();
         cy.get('.backdrop').should('not.exist');
         cy.get('dialog.modal').should('not.exist');
     });
